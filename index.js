190ea6b34bd460c9c19fae644add38163cb5b3fc
PopovVK <vladsmartvladsmart@gmail.com>
2023-05-25 13:23:44 +0300

    Final

---
+++ index.js
@@ -0,0 +1,12 @@
+const yourName = prompt('Enter a username')
+console.log(yourName)
+
+
+if (yourName) {
+    alert('Hello, ' + yourName + '! ' + 'How are you?')
+}
+else  {
+    alert('Error!')
+}
+
+
